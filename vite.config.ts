75d01da64901b6fa671f7ae12b2ea7fd0dbcc598
Xander Ward <xander@applydigital.co>
2026-02-27 14:41:23 -0500

    wrong end line

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/webrtc-multiplayer/';
+  base: '/webrtc-multiplayer/',
   plugins: [react()],
 })
